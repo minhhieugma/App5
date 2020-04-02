0c9c2fe9ee864921d8a172b81503afb5d2ff5de0
Hieu Le <hieu.le@hoozing.onmicrosoft.com>
2020-04-02 15:22:50 +0700

    Add project files.

---
+++ App5/App5.Shared/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App5
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var a = args.InvokedItemContainer.Tag;
+           // ContentFrame.Navigate(typeof(BlankPage1), null, args.RecommendedNavigationTransitionInfo);
+        }
+        private void NavView_BackRequested(NavigationView sender,
+                                  NavigationViewBackRequestedEventArgs args)
+        {
+
+        }
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
